8eabb31b62139d5c330aceb7338eff7174087bb8
Xiaoyi Liu <lxy878@gmail.com>
2020-03-13 13:15:58 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,16 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance
+  extend MetaDancing
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
